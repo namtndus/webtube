c5b3fa06ef0e7a848899495995260a8a3f4eda62
namtndus <rnrmf0721@gmail.com>
2021-05-27 00:37:16 +0900

    create pug file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,38 @@
-export const trending = (req, res) => res.send("Trending")
+const fakeUser = {
+    username: "suyeon",
+    loggedIn: true
+}
+
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        id: 1
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        id: 2
+    }, {
+        title: "third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        id: 1
+    }
+];
+
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: 'Home', fakeUser, videos })
+};
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => {
-    res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    res.send("Edit Video");
-}
+export const see = (req, res) => res.render("watch", { pageTitle: 'Watch' });
+export const edit = (req, res) => res.render("edit", { pageTitle: 'Edit' });
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
     return res.send("Delete Video");
-}
+};
